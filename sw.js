3e6ef3ad03c6938cd2ec9bb9b691b1ebb36eca31
vf12776-ux <vf12776-ux@users.noreply.github.com>
2026-05-03 15:56:47 +0300

    Final PWA with install prompt

--- sw.js
+++ sw.js
@@ -1,21 +1,28 @@
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open('alkobufet-v1').then((cache) => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/manifest.json',
-                '/icon-192.png',
-                '/icon-512.png'
-            ]);
-        })
-    );
+const CACHE = 'alko-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE).map(key => caches.delete(key))
+    ))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
 });
